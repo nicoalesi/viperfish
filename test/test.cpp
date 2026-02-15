a6f1b1253abba45b2cce0af11b79405de468f6b3
nicoalesi <nicoalesi.cs@gmail.com>
2026-02-15 15:59:34 +0100

    new: test positions
    Add different positions for PERFT and pass all of them.

--- test/test.cpp
+++ test/test.cpp
@@ -12,7 +12,7 @@ int main (void) {
     if (run_attack_gen_tests()) exit(EXIT_FAILURE);
     if (run_move_gen_tests()) exit(EXIT_FAILURE);
     if (run_fen_tests()) exit(EXIT_FAILURE);
-    parse_fen(position1);
+    parse_fen(position4);
     // make_move(encode_move(b4, a4, R, 0, 0, 0, 0, 0));
     // make_move(encode_move(d6, d5, p, 0, 0, 0, 0, 0));
     // make_move(encode_move(a5, b4, K, 0, 0, 0, 0, 0));
@@ -20,7 +20,7 @@ int main (void) {
     // make_move(encode_move(a4, a6, R, 0, 0, 0, 0, 0));
     // make_move(encode_move(c7, c5, p, 0, 0, 1, 0, 0));
     print_board();
-    run_perft(8);
+    run_perft(5);
     std::cout << "captures: " << capturess << "\n";
     std::cout << "checks: " << checkss << "\n";
 }
